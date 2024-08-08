9acdbc8c63c77bd2e1dcffad8e0896b918b4955a
jill-gosrani <jillgosrani15@gmail.com>
2024-08-07 17:22:21 -0700

    Updated data.sql and car pogo

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into car (car_model, car_name, car_number, insurance_type, is_available, is_insured, price, type) values 
+('Anniversary edition', 'Ertiga', 'MH 04 9641', 'Standard', TRUE, TRUE, 1500000.0, 'SUV'), 
+('Basic', 'Ritz', 'MH 03 9641', 'premium', TRUE, TRUE, 500000.0, 'COMPACT'),
+('XYZ', 'ABC', 'WKA 23LN', null, TRUE, FALSE, 100000.0, 'TEST');
